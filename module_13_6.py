fe3ef3ee7c85cdab49a2a62e4ec1339203ef1be6
Dmitriy-75 <dimakasimov29@gmail.com>
2024-10-29 23:28:39 +0300

    Add files via upload
    Домашнее задание по теме "Инлайн клавиатуры".
    Цель: научится создавать Inline клавиатуры и кнопки на них в Telegram-bot.
    
    Задача "Ещё больше выбора":
    Необходимо дополнить код предыдущей задачи, чтобы при нажатии на кнопку 'Рассчитать' присылалась Inline-клавиатруа.
    Создайте клавиатуру InlineKeyboardMarkup с 2 кнопками InlineKeyboardButton:
    С текстом 'Рассчитать норму калорий' и callback_data='calories'
    С текстом 'Формулы расчёта' и callback_data='formulas'
    Создайте новую функцию main_menu(message), которая:
    Будет обёрнута в декоратор message_handler, срабатывающий при передаче текста 'Рассчитать'.
    Сама функция будет присылать ранее созданное Inline меню и текст 'Выберите опцию:'
    Создайте новую функцию get_formulas(call), которая:
    Будет обёрнута в декоратор callback_query_handler, который будет реагировать на текст 'formulas'.
    Будет присылать сообщение с формулой Миффлина-Сан Жеора.
    Измените функцию set_age и декоратор для неё:
    Декоратор смените на callback_query_handler, который будет реагировать на текст 'calories'.
    Теперь функция принимает не message, а call. Доступ к сообщению будет следующим - call.message.
    По итогу получится следующий алгоритм:
    Вводится команда /start
    На эту команду присылается обычное меню: 'Рассчитать' и 'Информация'.
    В ответ на кнопку 'Рассчитать' присылается Inline меню: 'Рассчитать норму калорий' и 'Формулы расчёта'
    По Inline кнопке 'Формулы расчёта' присылается сообщение с формулой.
    По Inline кнопке 'Рассчитать норму калорий' начинает работать машина состояний по цепочке.

---
+++ module_13_6.py
@@ -0,0 +1,131 @@
+# Домашнее задание по теме "Инлайн клавиатуры".
+# Цель: научится создавать Inline клавиатуры и кнопки на них в Telegram-bot.
+#
+# Задача "Ещё больше выбора":
+# Необходимо дополнить код предыдущей задачи, чтобы при нажатии на кнопку 'Рассчитать' присылалась Inline-клавиатруа.
+# Создайте клавиатуру InlineKeyboardMarkup с 2 кнопками InlineKeyboardButton:
+# С текстом 'Рассчитать норму калорий' и callback_data='calories'
+# С текстом 'Формулы расчёта' и callback_data='formulas'
+# Создайте новую функцию main_menu(message), которая:
+# Будет обёрнута в декоратор message_handler, срабатывающий при передаче текста 'Рассчитать'.
+# Сама функция будет присылать ранее созданное Inline меню и текст 'Выберите опцию:'
+# Создайте новую функцию get_formulas(call), которая:
+# Будет обёрнута в декоратор callback_query_handler, который будет реагировать на текст 'formulas'.
+# Будет присылать сообщение с формулой Миффлина-Сан Жеора.
+# Измените функцию set_age и декоратор для неё:
+# Декоратор смените на callback_query_handler, который будет реагировать на текст 'calories'.
+# Теперь функция принимает не message, а call. Доступ к сообщению будет следующим - call.message.
+# По итогу получится следующий алгоритм:
+# Вводится команда /start
+# На эту команду присылается обычное меню: 'Рассчитать' и 'Информация'.
+# В ответ на кнопку 'Рассчитать' присылается Inline меню: 'Рассчитать норму калорий' и 'Формулы расчёта'
+# По Inline кнопке 'Формулы расчёта' присылается сообщение с формулой.
+# По Inline кнопке 'Рассчитать норму калорий' начинает работать машина состояний по цепочке.
+
+
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+api = 'Ключ'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button1, button2)
+
+
+
+ki = InlineKeyboardMarkup()
+i_button1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+i_button2 = InlineKeyboardButton(text='Формулы расчета', callback_data='formulas')
+ki.add(i_button1,i_button2)
+
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет. Я бот, помогающий твоему здоровью', reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
+async def  main_menu(message):
+    await message.answer(text='Выберите опцию', reply_markup=ki)
+
+
+@dp.callback_query_handler(text = 'formulas')
+async def get_formulas(call):
+    await call.message.answer('10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+
+
+
+@dp.callback_query_handler(text = 'calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growht(message, state):
+    await state.update_data(age=int(message.text))
+    await message.answer(f"Введите свой рост")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=int(message.text))
+    await message.answer(f"Введите свой вес")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+
+    calories = 10*(data['weight']) + 6*(data['growth']) - 5*(data['age']) + 5
+
+    await message.answer(f"Ваша норма калорий = {calories}")
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start')
+
+
+if __name__ == '__main__':
+    executor. start_polling(dp, skip_updates=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
